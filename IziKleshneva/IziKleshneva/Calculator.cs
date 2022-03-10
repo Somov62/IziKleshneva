eb8c5436cd130201c88d7f8e7bc68f8718f1bbe2
Somov <misha2003980@gmail.com>
2022-03-10 23:41:29 +0300

    Добавьте файлы проекта.

---
+++ IziKleshneva/IziKleshneva/Calculator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IziKleshneva
+{
+    public static class Calculator
+    {
+        public static string SolvePrimer(string primer)
+        {
+            primer = primer.Replace(" ", "");
+            primer = primer.Replace("_", " ");
+            primer = primer.Replace(":", "÷");
+            primer = primer.Replace("*", "×");
+            //решаем факториал
+            while (primer.IndexOf("!") != -1)
+            {
+                int znakIndex = primer.IndexOf("!");
+                int leftBorder = LeftBorderIndex(primer, znakIndex);
+                double num = Convert.ToDouble(primer.Substring(leftBorder, znakIndex - leftBorder));
+                primer = primer.Remove(leftBorder, znakIndex - leftBorder + 1);
+                int rez = 1;
+                for (int i = 1; i <= num; i++)
+                {
+                    rez *= i;
+                }
+                primer = primer.Insert(leftBorder, rez.ToString());
+            }
+            //решаем степень
+            while (primer.IndexOf("^") != -1)
+            {
+                int znakIndex = primer.IndexOf("^");
+                int leftBorder = LeftBorderIndex(primer, znakIndex);
+                primer = NewPrimer(primer, leftBorder, znakIndex, out double fNum, out double sNum);
+                primer = primer.Insert(leftBorder, (Math.Pow(fNum, sNum)).ToString());
+            }
+
+            //решаем умножение/деление
+            while (primer.IndexOf("×") != -1 || primer.IndexOf("÷") != -1)
+            {
+                char[] simbols = { '×', '÷' };
+                int znakIndex = primer.IndexOfAny(simbols, 1);
+                int leftBorder = LeftBorderIndex(primer, znakIndex);
+                char znak = primer[znakIndex];
+                primer = NewPrimer(primer, leftBorder, znakIndex, out double fNum, out double sNum);
+                if (znak == '×') primer = primer.Insert(leftBorder, ((fNum * sNum)).ToString());
+                else primer = primer.Insert(leftBorder, ((fNum / sNum)).ToString());
+            }
+
+            //решаем сложение/вычитание
+            while (primer.IndexOf("+") != -1 || primer.IndexOf("-", 1) != -1)
+            {
+                char[] simbols = { '-', '+' };
+                int znakIndex = primer.IndexOfAny(simbols, 1);
+                int leftBorder = LeftBorderIndex(primer, znakIndex);
+                char znak = primer[znakIndex];
+                primer = NewPrimer(primer, leftBorder, znakIndex, out double fNum, out double sNum);
+                if (znak == '+') primer = primer.Insert(leftBorder, ((fNum + sNum)).ToString());
+                else primer = primer.Insert(leftBorder, ((fNum - sNum)).ToString());
+            }
+            return primer;
+        }
+        /// <summary>
+        /// Метод выделяющий из примера единичное действия типа "хRу", где R - знак арифметического действия. Метод наход x, y и возвращает их. 
+        /// Также вырезает из примера действие "xRy" для последующей вставки результата вычислений.
+        /// </summary>        
+        private static string NewPrimer(string primer, int lBorder, int i, out double fNum, out double sNum)
+        {
+            int rBorder = RightBorderIndex(primer, i);//находим правую границу правого от знака числа
+            fNum = Convert.ToDouble(primer.Substring(lBorder, i - lBorder));//получаем первое число
+            sNum = Convert.ToDouble(primer.Substring(i + 1, rBorder - i));//получаем второе
+            primer = primer.Remove(lBorder, rBorder - lBorder + 1);//вырезаем все действие
+            return primer;//возвращаем строку с вырезанным действием и два числа для последущего действия с ними
+        }
+        /// <summary>
+        /// Возвращает правую границу промежуточного действия при помощи индекса знака. Используется для последущего получения правого от знака числа методом Substring в методе NewPrimer
+        /// </summary>
+        private static int RightBorderIndex(string primer, int znakIndex)
+        {
+            int rightBorder = znakIndex + 1;//ищем правую границу, начиная от знака действия
+            if (primer[znakIndex + 1] == '-') znakIndex++;//если сразу после знака стоит минус, значит мы работаем с отрицательным числом
+            for (int i = znakIndex; i < primer.Length - 1; i++)
+            {
+                if (primer[i + 1] == ',' || primer[i + 1] == '.') continue;//запятая не влияет на границу, пропускаем ее
+                if (!Int32.TryParse(primer[i + 1].ToString(), out _))//если натыкаемся на знак или скобку - находим нашу границу
+                {
+                    rightBorder = i;
+                    break;
+                }
+                //если ни на что не наткнулись, за правую границу принимаем последний элемент строки
+                if (i + 1 == primer.Length - 1) rightBorder = i + 1;
+            }
+            return rightBorder;
+        }
+        private static int LeftBorderIndex(string primer, int znakIndex)
+        {
+            int leftBorder = znakIndex - 1;
+            for (int i = znakIndex; i > 0; i--)
+            {
+                if (primer[i - 1] == ',' || primer[i - 1] == '.') continue;
+                if (primer[i - 1] == '-' && i - 1 == 0)
+                {
+                    leftBorder = 0;
+                    break;
+                }
+                if (!Int32.TryParse(primer[i - 1].ToString(), out _))//если натыкаемся на знак или скобку - находим нашу границу
+                {
+                    leftBorder = i;
+                    break;
+                }
+                if (i - 1 == 0) leftBorder = 0;
+            }
+            return leftBorder;
+        }
+    }
+}
---
+++ IziKleshneva/IziKleshneva/MainPage.xaml.cs
@@ -0,0 +1,166 @@
+﻿using IziKleshneva;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+namespace IziKleshneva
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Solve_Click(object sender, EventArgs e)
+        {
+            string equlation = txtEquation.Text.Replace("=0", "");
+            equlation = equlation.Replace(" ", "");
+            if (!double.TryParse(txtInterval1.Text, out double interval1)
+                | !double.TryParse(txtInterval2.Text, out double interval2)
+                | !double.TryParse(txtEpsilon.Text, out double epsilon))
+            {
+                return;
+            }
+
+            while(step1Stack.Children.Count > 1)
+            {
+                step1Stack.Children.RemoveAt(1);
+            }
+            while (step2Stack.Children.Count > 1)
+            {
+                step2Stack.Children.RemoveAt(1);
+            }
+            while (step3Stack.Children.Count > 1)
+            {
+                step3Stack.Children.RemoveAt(1);
+            }
+
+            #region Step 1
+            step1Stack.Children.Add(new Label() { Text = "f(x) = " + equlation});
+            step1Stack.Children.Add(new Label() { Text = "a = " + interval1 });
+            step1Stack.Children.Add(new Label() { Text = "b = " + interval2 });
+            step1Stack.Children.Add(new Label() { Text = "Е = " + epsilon });
+            #endregion
+
+            #region Step 2
+            bool IsСonditionComplete = Step2(equlation, interval1, interval2);
+            if (!IsСonditionComplete) return;
+            #endregion
+
+            #region Step 3
+            //C = a + |b - a|
+            //        -------
+            //           2
+
+
+            double a = interval1;
+            double b = interval2;
+            
+
+            while (true)
+            {
+                double c = a + (Math.Abs(b - a) / 2);
+
+                double delta = Math.Abs(b - c);
+
+                step3Stack.Children.Add(new Label() { Text = $"C = {a} + |{b} - {a}| ÷ 2 = " + c});
+                step3Stack.Children.Add(new Label() { Text = DeltaToView(c, b, epsilon) });
+                if (delta <= epsilon)
+                {
+                    step3Stack.Children.Add(new Label() { Text = "Точность достигнута" });
+                    step3Stack.Children.Add(new Label() { Text = "Корень: " + c });
+                    break;
+                }
+                Label label = step3Stack.Children[step3Stack.Children.Count - 1] as Label;
+                label.Text += " (Можно не писать)";
+                label.TextColor = Color.Gray;
+                string f1 = FormulaToView(equlation, a);
+                string f2 = FormulaToView(equlation, c);
+                string f3 = FormulaToView(equlation, b);
+                step3Stack.Children.Add(new Label() { Text = f1 });
+                step3Stack.Children.Add(new Label() { Text = f2 });
+                step3Stack.Children.Add(new Label() { Text = f3 });
+
+                if (CheckConvergenceCondition(f1, f2))
+                {
+                    b = c;
+                }
+                else
+                {
+                   a = c;
+                }
+                step3Stack.Children.Add(new Label() { Text = $"Следующий отрезок: [{a} ; {b}]" });
+                step3Stack.Children.Add(new Label() { Text = $" " });
+            }
+
+            #endregion
+
+        }
+
+        private bool Step2(string equlation, double interval1, double interval2)
+        {
+            string f1 = FormulaToView(equlation, interval1);
+            string f2 = FormulaToView(equlation, interval2);
+            step2Stack.Children.Add(new Label() { Text = f1 });
+            step2Stack.Children.Add(new Label() { Text = f2 });
+            if (CheckConvergenceCondition(f1, f2))
+            {
+                step2Stack.Children.Add(new Label() { Text = "Условие на сходимость выполнено", FontSize = 18 });
+                return true;
+            }
+            step2Stack.Children.Add(new Label() { Text = "Условие на сходимость не выполнено", FontSize = 18 });
+            return false;
+        }
+
+        private string FormulaToView(string equlation, double value)
+        {
+            string primer = equlation.Replace("x", value.ToString());
+            double result1 = Convert.ToDouble(Calculator.SolvePrimer(primer));
+            result1 = Math.Round(result1, 4);
+            string sxod1 = "";
+            if (result1 < 0) sxod1 = " < 0";
+            if (result1 > 0) sxod1 = " > 0";
+            return $"f({value}) = {primer} = " + result1 + sxod1;
+        }
+
+        private string DeltaToView(double c1, double c2, double epsilon)
+        {
+            double result1 = Math.Abs(c2 - c1);
+            result1 = Math.Round(result1, 4);
+            string sxod1 = "";
+            if (result1 < epsilon) sxod1 = " < " + epsilon;
+            if (result1 > epsilon) sxod1 = " > " + epsilon;
+            return $"ΔС = |{c2} - {c1}| = " + result1 + sxod1;
+        }
+
+        private bool CheckConvergenceCondition(string f1, string f2)
+       {
+            if ((f1.Contains("<") && f2.Contains(">")) || (f1.Contains(">") && f2.Contains("<")))
+            {
+                return true;
+            }
+            return false;
+       }
+
+        private void txtEquation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length == 0) return;
+            txtEquation.Text = e.NewTextValue;
+            char[] symbols = {'^', '*', '/', '.', ',', '+', '=', '-', 'x' };
+            for (int i = 0; i < txtEquation.Text.Length; i++)
+            {
+                if (char.IsDigit(txtEquation.Text[i])) continue;
+                if (symbols.Contains(txtEquation.Text[i])) continue;
+                txtEquation.Text = txtEquation.Text.Remove(i);
+            }
+        }
+    }
+
+}
